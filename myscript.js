913dbdc3090d76940a761a6376b62231a534db88
Anthony Barrera <anthony.j.barrera@gmail.com>
2017-08-31 16:30:05 -0400

    Add span for popover around some numbers.

--- myscript.js
+++ myscript.js
@@ -1,9 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var rxp = new RegExp("([0-9]+)", "gm");
+$("div").contents().filter(function () {
+    return this.nodeType === 3;
+}).each(function () {
+    var text = this.nodeValue;
+    $(this).replaceWith(text.replace(rxp, "<span data-toggle='popover'>$1</span>"));
+});
 
-
-var element = document.getElementsByClassName("jsb");
-document.body.style.backgroundColor = "red";
+$(document).on('click', 'span[data-toggle="popover"]', function () {
+    $(this).popover({
+        content: $(this).text(),
+        trigger: "hover",
+        placement: "top"});
+    $(this).popover("show");
+});
