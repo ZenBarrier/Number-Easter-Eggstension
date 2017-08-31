ec64d2df8082df9c84d0231cedccd7b8193fe018
Anthony Barrera <anthony.j.barrera@gmail.com>
2017-08-31 16:49:22 -0400

    Change span into custom <egg> tag to prevent breaking style.

--- myscript.js
+++ myscript.js
@@ -1,12 +1,12 @@
 var rxp = new RegExp("([0-9]+)", "gm");
-$("div").contents().filter(function () {
+$("div, p, span, pre, b, i, h, code").not(".easterEggstension").contents().filter(function () {
     return this.nodeType === 3;
 }).each(function () {
     var text = this.nodeValue;
-    $(this).replaceWith(text.replace(rxp, "<span data-toggle='popover'>$1</span>"));
+    $(this).replaceWith(text.replace(rxp, "<egg class='easterEggstension' data-toggle='popover'>$1</egg>"));
 });
 
-$(document).on('click', 'span[data-toggle="popover"]', function () {
+$(document).on('click', 'egg[data-toggle="popover"]', function () {
     $(this).popover({
         content: $(this).text(),
         trigger: "hover",
