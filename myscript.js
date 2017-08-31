f93eff4fdea7775f37bb47be74592f5f9948736b
Anthony Barrera <anthony.j.barrera@gmail.com>
2017-08-31 17:22:47 -0400

    Wrapped script in function.

--- myscript.js
+++ myscript.js
@@ -1,15 +1,19 @@
-var rxp = new RegExp("([0-9]+)", "gm");
-$("div, p, span, b, i, h").not(".easterEggstension").contents().filter(function () {
-    return this.nodeType === 3;
-}).each(function () {
-    var text = this.nodeValue;
-    $(this).replaceWith(text.replace(rxp, "<egg class='easterEggstension' data-toggle='popover'>$1</egg>"));
-});
+(function () {
 
-$(document).on('click', 'egg[data-toggle="popover"]', function () {
-    $(this).popover({
-        content: $(this).text(),
-        trigger: "hover",
-        placement: "top"});
-    $(this).popover("show");
-});
+    var rxp = new RegExp("([0-9]+)", "gm");
+    $("div, p, span, b, i, h").not(".easterEggstension").contents().filter(function () {
+        return this.nodeType === 3;
+    }).each(function () {
+        var text = this.nodeValue;
+        $(this).replaceWith(text.replace(rxp, "<egg class='easterEggstension' data-toggle='popover'>$1</egg>"));
+    });
+
+    $(document).on('click', 'egg[data-toggle="popover"]', function () {
+        $(this).popover({
+            content: $(this).text(),
+            trigger: "hover",
+            placement: "top"});
+        $(this).popover("show");
+    });
+
+})();
