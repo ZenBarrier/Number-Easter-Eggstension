7614166195b5aa8c26e51af1df88c2c33af9d12a
Anthony Barrera <anthony.j.barrera@gmail.com>
2017-10-27 15:32:55 -0400

    Background Script that makes api call.

---
+++ background.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+chrome.runtime.onMessage.addListener(
+        function (number, sender, sendResponse) {
+
+            var searchUrl = 'http://numbersapi.com/' + number;
+            
+            var x = new XMLHttpRequest();
+            x.onload = function () {
+                sendResponse(x.responseText);
+            };
+            x.onerror = function () {
+                console.log(x);
+                sendResponse("");
+            };
+            x.open('GET', searchUrl, true);
+            x.send();
+            console.log(x);
+            return true;
+        }
+);
+
